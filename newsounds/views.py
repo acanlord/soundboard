8d1268dfdc0c48f9a35cfb2218e9782f02b72659
Richard Houck <rdhouck@gmail.com>
2019-12-05 17:38:08 -0800

    updated stuff

--- newsounds/views.py
+++ newsounds/views.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from django.http import HttpResponse
 from django.shortcuts import render
 # from django.contrib.auth.models import User
@@ -12,13 +13,22 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
 
+# def index(request):
+#     post = AudioFile.objects.create(
+#         #absolute_path="/some/path/to/a/file.wav",
+#         absolute_path="{ path_to_file }",
+#         filename="{ file_name }"
+#     )
+
 def index(request):
     post = AudioFile.objects.create(
-        #absolute_path="/some/path/to/a/file.wav",
-        absolute_path="{ path_to_file }",
-        filename="{ file_name }"
+        save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', request.FILES['file'])
+        path = default_storage.save(save_path, request.FILES['file'])
+        #return default_storage.path(path)
     )
-    all_audio_files = AudioFile.objects.all()
+
+
+     all_audio_files = AudioFile.objects.all()
     context = {
         'audio_files' : all_audio_files
     }
